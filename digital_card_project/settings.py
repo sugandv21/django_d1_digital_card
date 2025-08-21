c59b572fad6204ffd407198d3259af88231a0364
sugandv21 <sdvsuganya@gmail.com>
2025-08-21 11:21:56 +0530

    Update settings.py

--- digital_card_project/settings.py
+++ digital_card_project/settings.py
@@ -116,10 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# Static files
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "profile_app/static"]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "profile_app/static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -127,4 +126,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
